ce0d1d7841f4f39c6ff79c40dfa74a67d4a9104a
IgorSilvares <igorsilvares@gmail.com>
2023-04-12 12:06:53 +0200

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id": '1',
+    "title": 'ANGULAR FRAMEWORK',
+    "description":
+      'A Single-Page-Application blog created using Angular on the DIO.me bootcamp. Angular is Framework which is used for creating Fast Web Applications. It uses concepts of SPA in which UI is delivered in the beginning of application request and later only data is requested which makes SPA applications fast.',
+    "photoCover":
+      'https://th.bing.com/th/id/OIP.kuRzuMuDyDaqAAglpORmMQHaE8?w=284&h=180&c=7&r=0&o=5&dpr=2&pid=1.7',
+
+  },
+  {
+    "id": '2',
+    "title": 'TypeScript offers all of JavaScript’s features, and an additional layer on top of these',
+    "description": 'TypeScript offers all of JavaScript’s features, and an additional layer on top of these',
+    "photoCover": 'https://th.bing.com/th/id/OIP.MYg4MkoNTs5CrBCBOpJyBAHaEF?w=314&h=180&c=7&r=0&o=5&dpr=2&pid=1.7',
+  },
+  {
+    "id": '3',
+    "title": 'HTML is the standard markup language for Web pages.',
+    "description": 'HTML is the standard markup language for Web pages.',
+    "photoCover": 'https://th.bing.com/th/id/OIP.o-wNqCyhGc3XpFMfCCFpigHaEK?w=309&h=180&c=7&r=0&o=5&dpr=2&pid=1.7',
+  },
+  {
+    "id": '4',
+    "title": 'Cascading Style Sheets (CSS) is used to format the layout of a webpage',
+    "description": 'Cascading Style Sheets (CSS) is used to format the layout of a webpage',
+    "photoCover": 'https://th.bing.com/th/id/OIP.3ZzgCdy_Jkjo17ro3VyG6gHaEP?w=287&h=180&c=7&r=0&o=5&dpr=2&pid=1.7',
+  },
+];
